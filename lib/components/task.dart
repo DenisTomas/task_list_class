a04f782d046d18c78ca75fa272bff469d70a4566
Denis Tomás <denistomas03@gmail.com>
2022-09-26 17:05:06 -0300

    Refactored and added new screen (add new task).

---
+++ lib/components/task.dart
@@ -0,0 +1,127 @@
+import 'package:aula_projeto_inicial/components/difficulty.dart';
+import 'package:flutter/material.dart';
+
+class Task extends StatefulWidget {
+  final String name;
+  final String photo;
+  final int difficulty;
+
+  const Task(this.name, this.photo, this.difficulty, {Key? key})
+      : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int lvl = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.blue,
+            ),
+            height: 140,
+          ),
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.white70,
+                ),
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.black12,
+                      ),
+                      width: 72,
+                      height: 100,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.asset(
+                          widget.photo,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                            width: 200,
+                            child: Text(
+                              widget.name,
+                              style: const TextStyle(
+                                fontSize: 24,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            )),
+                        Difficulty(
+                          difficultyLevel: widget.difficulty,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 52,
+                      width: 52,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              lvl++;
+                            });
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: const [
+                              Icon(Icons.arrow_drop_up_outlined),
+                              Text(
+                                'UP',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ],
+                          )),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        color: Colors.white70,
+                        value: (widget.difficulty > 0)
+                            ? (lvl / widget.difficulty) / 10
+                            : 1,
+                      ),
+                    ),
+                    Text(
+                      'Lvl: $lvl',
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,62 @@
+import 'package:aula_projeto_inicial/components/task.dart';
+import 'package:flutter/material.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool opacity = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: const Text('Tasks'),
+      ),
+      body: AnimatedOpacity(
+        opacity: opacity ? 1 : 0,
+        duration: const Duration(milliseconds: 500),
+        child: ListView(
+          children: const [
+            Task(
+                'Learn Flutter',
+                'assets/images/st,small,507x507-pad,600x600,f8f8f8.jpg',
+                5),
+            Task(
+                'Play Bass',
+                'assets/images/new_bass_comp.jpg',
+                5),
+            Task(
+                'Clean the House',
+                'assets/images/shutterstock_557357668.webp',
+                3),
+            Task(
+                'Cook Lunch',
+                'assets/images/chef-cooking-dinner-at-table-in-kitchen-vector-32060450.jpg',
+                2),
+            Task(
+                'Sleep',
+                'assets/images/283fb478-5554-44aa-b37f-7917a90f38d7.jpg',
+                4),
+            SizedBox(
+              height: 80,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            opacity = !opacity;
+          });
+        },
+        child: const Icon(Icons.remove_red_eye),
+      ),
+    );
+  }
+}
